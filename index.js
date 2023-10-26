d82ed8be9afdeac9ed983bcda7e856451973a852
kuronight <85890469+kuronight29@users.noreply.github.com>
2023-10-26 15:49:11 +0700

    up lệnh dai;y

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, MessageAttachment } = require('discord.js-selfbot-v13');
 const { token } = require('./config.json');
 const client = new Client({});
+const schedule = require('node-schedule');
 
 client.on('ready', async () => {
   console.log(`${client.user.username} is ready!`);
@@ -14,8 +15,15 @@ client.on('messageCreate', async message => {
     setTimeout(async () => {
         console.log(`${client.user.username} đã work`);
         await message.channel.sendSlash('1165870665304379402', 'work');
-    }, 3600000); 
+    }, 3600000);
   }
 });
 
+schedule.scheduleJob('0 0 * * *', async function(){
+  const channel = client.channels.cache.get('1165513340697718814');
+  console.log(`${client.user.username} đã nhận daily`);
+  await channel.sendSlash('1165870665304379402', 'daily');
+});
+
 client.login(token);
+
